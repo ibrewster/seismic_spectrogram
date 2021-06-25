8d2d7bb6c1a492369eece0255b57a5359f54d87b
Israel Brewster <ijbrewster@alaska.edu>
2021-06-25 15:06:08 -0800

    Work on docker implementation

--- run_web.py
+++ run_web.py
@@ -2,4 +2,4 @@ from specweb import app
 
 if __name__ == "__main__":
     app.config['TEMPLATES_AUTO_RELOAD'] = True
-    app.run(use_reloader = True, debug = False)
+    app.run(use_reloader = True, debug = False, host = "0.0.0.0")
--- setup.py
+++ setup.py
@@ -6,9 +6,12 @@ import gen_config
 import gen_station_config
 
 if __name__ == "__main__":
+    assume_yes = False
+    if len(sys.argv) > 1 and sys.argv[1] == '-y':
+        assume_yes = True
     print("Welcome to the spectrogram web setup script!")
     print("This script will walk you through the steps needed to get up and running.")
-    cont = 'invalid'
+    cont = 'y' if assume_yes else 'invalid'
     while cont.lower() not in ['', 'y', 'n']:
         cont = input("Continue? [Y/n]: ")
 
@@ -19,7 +22,7 @@ if __name__ == "__main__":
     subprocess.check_call([sys.executable, "-m", "pip", "install", "--upgrade",
                            "-r", "requirements.txt"])
 
-    gen_base_conf = 'invalid'
+    gen_base_conf = 'y' if assume_yes else 'invalid'
     while gen_base_conf.lower() not in ('', 'y', 'n'):
         gen_base_conf = input("Generate new config.ini file? [Y/n]: ")
 
@@ -28,16 +31,19 @@ if __name__ == "__main__":
         print("specgen/config.ini has been generated.")
         print("Please verify the contents of this file and edit")
         print("To suit your enviroment before continuing.")
-        input("Hit return when ready to continue.")
+        if not assume_yes:
+            input("Hit return when ready to continue.")
 
-    gen_station = 'invalid'
+    gen_station = 'y' if assume_yes else 'invalid'
     while gen_station.lower() not in ('', 'n', 'y'):
         gen_station = input("Generate new station config file? [Y/n]: ")
 
     if gen_station.lower() != 'n':
         print("Please verify that the settings and VOLCS list at the top of the")
-        print("gen_station_config.py file")
-        input("Hit return when ready to continue")
+        print("gen_station_config.py file are as desired")
+        if not assume_yes:
+            input("Hit return when ready to continue")
+
         print("Generating station config. This may take a few minutes...")
         gen_station_config.generate_stations()
         print("Station config generated")
@@ -49,7 +55,7 @@ if __name__ == "__main__":
     print()
     print("This step is optional, if a module required for a hook is not installed")
     print("that hook will simply not be run.")
-    install_hooks = 'invalid'
+    install_hooks = 'y' if assume_yes else 'invalid'
     while install_hooks.lower() not in ('', 'n', 'y'):
         install_hooks = input("Install required modules for hooks? [Y/n]: ")
 
